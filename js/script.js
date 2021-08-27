f9caa1148b7026d11dd08f509f823afca69503dc
Rauf <Raufgad@gmail.com>
2021-08-27 15:06:32 +0300

    Сделал даже, кручес, чем в туториале

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 ' use strict';
-const isChecked = true; 
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?'),
+movieName = prompt('Один из последних просмотренных фильмов?'),
+movieRate = prompt('На сколько оцените его?'),
+movieName1 = prompt('Один из последних просмотренных фильмов?'),
+movieRate1 = prompt('На сколько оцените его?');
+
+   
+const personalMovieDB = {
+    count: numberOfFilms = {}, 
+    movies: {},
+    actors: {},
+    genres:[],
+    privat:false
+};
+personalMovieDB.movies[`"${movieName}"`] = (movieRate);
+personalMovieDB.movies[`"${movieName1}"`] = (movieRate1);
+
+console.log(personalMovieDB.movies);
+
+
+
+
+
