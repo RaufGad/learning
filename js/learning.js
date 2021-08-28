465f1e604d04e33ffeeca191a109a4e250f5a934
Rauf <Raufgad@gmail.com>
2021-08-28 21:18:10 +0300

    выполнил второе ДЗ

--- js/learning.js
+++ js/learning.js
@@ -82,6 +82,5 @@ for (let i = 1; i < 10; i++){
     }
 console.log(i);
 }
-*//
-
+*/
 
--- js/practice.js
+++ js/practice.js
@@ -1,23 +1,36 @@
 ' use strict';
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
+if ( numberOfFilms < 10) {
+    alert ("Просмотрено довольно мало фильмов");
+}else if ( numberOfFilms < 31 ) {
+    alert ("Вы классический зритель");
+}else if ( numberOfFilms > 30 ) {
+    alert ("Вы киноман");
+}else {
+    alert("Произошла ошибка");
+}
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
-
 };
 
-const a = prompt ('Один из последних просмотренных фильмов?'),
-    b = prompt ('На сколько оцените его?'),
-    c = prompt ('Один из последних просмотренных фильмов?'),
-    d = prompt ('На сколько оцените его?');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    let num = 0;
+    while (num < 2) {
+        const a = prompt ('Один из последних просмотренных фильмов?'),
+              b = prompt ('На сколько оцените его?');
+        personalMovieDB.movies[a] = b;
+        if (a.length < 50 && b.length < 50) {
+            if  (a != "" && b !="") {
+                if (a !== null && b !== null){
+                    num++;
+                }
+            }
+        }
+}
 
 console.log (personalMovieDB);
-
-    
