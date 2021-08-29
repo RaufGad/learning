8df8e08e741877f02cf76d76968af1e2e13ffde1
Rauf <Raufgad@gmail.com>
2021-08-29 18:24:55 +0300

    прошел функции и методы

--- js/learning.js
+++ js/learning.js
@@ -84,3 +84,46 @@ console.log(i);
 }
 */
 
+//------------------ФУНКЦИИ------------
+
+let num = 20;
+
+function showFirstMessage(text){
+    console.log(text);
+    let num =  30;
+    console.log(num);
+}
+
+showFirstMessage('Hello World!');
+console.log(num);
+
+function calc (a,b){
+    return (a + b);
+}
+console.log(calc(10,11));
+console.log(calc(120,121));
+console.log(calc(10222,1334441));
+
+function ret(){
+    let num = 50;
+    //
+    return (num);
+}
+const anotherNum = ret();
+console.log (anotherNum);
+
+const logger = function(){
+    console.log('Hello');
+};
+logger();
+
+const calcu = (a,b) => { 
+    return a + b;
+};
+
+//------------------МЕТОДЫ И СВОЙСТВА СТРОК-----------------
+
+const str ='test';
+const arr = [1,2,3];
+console.log(arr.length);
+
